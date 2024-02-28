f5fe57c9d001557e4cd45a674b26a1e6ea72c8d9
Ben Aziel <70983779+destrospooder@users.noreply.github.com>
2024-02-28 05:50:43 -0500

    wow i really need to refactor this

--- 2_airfoilDNS/airfoilDNS_svd.m
+++ 2_airfoilDNS/airfoilDNS_svd.m
@@ -1,10 +1,8 @@
 %{
-
 ME493 - Methods of Data-Driven Control
-POD analysis of Low-Reynolds-number pitching airfoil 
-    direct numerical simulations
 Benjamin Aziel
 
+really needs refactoring
 %}
 
 clear; clc;
@@ -93,13 +91,77 @@ sgtitle(['temporal amplitudes']);
 %%
 
 % Let's attempt reconstruction for rank r
-r = 125;
+r = 4;
 X_approx = U(:, 1:r) * S(1:r, 1:r) * V(:, 1:r)';
 
-t_star = 200; % timestep of interest (max 401)
-ux_approx = reshape(X_approx(1:nx*ny, t_star), nx, ny);
+t_star = linspace(1, nt, 6); % timestep of interest (max r). will pick these better based on temporal amplitudes later
 
 figure;
-contourf(x, y, ux_approx', linspace(-0.4, 0.2, 120));
-caxis;
-colorbar;
+for k = 1:length(t_star)
+    subplot(2, 3, k);
+    ux_approx = reshape(X_approx(1:nx*ny, t_star(k)), nx, ny);
+    contourf(x, y, ux_approx', linspace(-0.4, 0.2, 40));
+    hold on;
+    title(t_star(k))
+    caxis([-0.4, 0.2]);
+    colorbar;
+    plot(xa(:,:),ya(:,:),'r-');  % plot all airfoil locations
+end
+plot(xa(:,:),ya(:,:),'k-')  % plot all airfoil locations
+sgtitle(['ux reconstruction - rank 4']);
+
+figure;
+for k = 1:length(t_star)
+    subplot(2, 3, k);
+    uy_approx = reshape(X_approx(nx*ny+1:end, t_star(k)), nx, ny);
+    contourf(x, y, uy_approx', linspace(-0.15, 0.15, 40));
+    hold on;
+    title(t_star(k))
+    caxis([-0.15, 0.15]);
+    colorbar;
+    plot(xa(:,:),ya(:,:),'r-');  % plot all airfoil locations
+end
+sgtitle(['uy reconstruction - rank 4']);
+
+%%
+% DMD time
+
+tv = 0; % truncation value
+
+params_file = fullfile('airfoilDNS_parameters.h5');
+dt_field = h5read(params_file, '/dt_field');
+
+[Phi, Lambda, Atilde, Amplitudes] = calc_dmd(X, tv);
+
+Eigscts = log(Lambda) / dt_field;
+
+figure;
+stem(imag(Eigscts)/(2*pi), abs(Amplitudes.*(Lambda))/max(abs(Amplitudes.*(Lambda))),'-','linewidth',2);
+xlim([-0.02 1.5]);
+ylim([0 1.2]);
+xlabel('Frequency'), ylabel('DMD mode amplitude (scaled)');
+
+%%
+% SINDy time!
+
+temp_amps = [V(:, 1:6) * S(1:6, 1:6)]; % x
+t_field; % t
+
+dtemp_amps = diff(temp_amps);
+dtemp_amps = vertcat(dtemp_amps, dtemp_amps(400, :)); % to fill it out
+
+polyorder = 2;
+n = 6; % dofs
+Theta = poolData(temp_amps, n, polyorder);
+m = size(Theta,2);
+
+lambda = 0.025;      % lambda is our sparsification knob.
+Xi = sparsifyDynamics(Theta, dtemp_amps, lambda, n);
+poolDataLIST({'x1', 'x2', 'x3', 'x4', 'x5', 'x6'}, Xi, n, polyorder);
+
+%%
+syms aleph
+plot(t_field - 50, SV(1)*V(:, 1))
+xlim([0 40])
+hold on
+fplot(0.0347/2 * aleph^2)
