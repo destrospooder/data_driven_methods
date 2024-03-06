cf4abf8ab7c375f45c93a657e00fc4232abaadd6
Ben Aziel <70983779+destrospooder@users.noreply.github.com>
2024-03-06 07:31:27 -0500

    final push of the morning

--- 3_airfoilDNS_refactor/var_import.py
+++ 3_airfoilDNS_refactor/var_import.py
@@ -2,6 +2,7 @@
 
 # just moving all my imported variables from the airfoil dataset here 
 # so it's easier to grab em for dmd/sindy later
+# honestly might use a yaml file later haha
 
 import h5py
 import numpy as np
@@ -9,16 +10,11 @@ import numpy as np
 grid_file = h5py.File('dataset/airfoilDNS_grid.h5', 'r+')
 data_file = h5py.File('dataset/airfoilDNS_a25f0p05.h5', 'r+')
 
-# grid coords
+# spatial grid coords
 x = np.squeeze(grid_file['x'][()])
 y = np.squeeze(grid_file['y'][()])
 nx = len(x)
 ny = len(y)
-
-# timesteps/timespan
-t_field = np.squeeze(data_file['t_field'][()])
-t_force = np.squeeze(data_file['t_force'][()])
-nt = len(t_field)
 
 # streamwise/transverse velocity
 ux = np.squeeze(data_file['ux'][()])
@@ -27,3 +23,6 @@ uy = np.squeeze(data_file['uy'][()])
 # airfoil coords
 xa = np.squeeze(data_file['xa'][()]) 
 ya = np.squeeze(data_file['ya'][()])
+t_field = np.squeeze(data_file['t_field'][()])
+t_force = np.squeeze(data_file['t_force'][()])
+nt = len(t_field)
