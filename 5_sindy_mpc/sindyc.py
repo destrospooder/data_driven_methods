e711b0b9c8118ca3c4bf28cc5975b135f2b3189f
Ben Aziel <benjamin.aziel@cooper.edu>
2024-06-03 13:11:12 -0400

    some edits + chapter 4 done

--- 5_sindy_mpc/sindyc.py
+++ 5_sindy_mpc/sindyc.py
@@ -14,18 +14,18 @@ import pysindy as ps
 from scipy.integrate import solve_ivp
 
 m = 1 # kg
-b = 2 # kg/s
-k = 3 # N/m
+b = 0.4 # kg/s
+k = 1 # N/m
 
 def u(t):
-    return 0.02 * np.sin(t)
+    return 2
 
 def mss(t, x, u):
     F = u(t)
     return(np.array([x[1], -k/m * x[0] - b/m * x[1] + F/m]))
 
 x0 = [1, 0]
-t_span = [0, 40]
+t_span = [0, 10]
 t_eval = np.linspace(t_span[0], t_span[1], 10000)
 
 data = solve_ivp(mss, 
@@ -51,23 +51,23 @@ ax[1].set_ylabel('Velocity [m/s]')
 # plt.show()
 
 # just for funsies
-noise = np.random.normal(0, 1e-2, np.shape(data.y))
+noise = np.random.normal(0, 1e-3, np.shape(data.y))
 
 fig, ax = plt.subplots(1, 2)
 
 ax[0].scatter(data.t, data.y[0] + noise[0, :])
-ax[0].set_title('Position vs. Time (Gaussian Noise, StdDev 1e-2)')
+ax[0].set_title('Position vs. Time (Gaussian Noise, StdDev 1e-3)')
 ax[0].set_xlabel('Time [s]')
 ax[0].set_ylabel('Position [m]')
 
 ax[1].scatter(data.t, data.y[1] + noise[1, :])
-ax[1].set_title('Velocity vs. Time (Gaussian Noise, StdDev 1e-2)')
+ax[1].set_title('Velocity vs. Time (Gaussian Noise, StdDev 1e-3)')
 ax[1].set_xlabel('Time [s]')
 ax[1].set_ylabel('Velocity [m/s]')
 
 # plt.show()
 
-knob = 0.9
+knob = 0.75
 
 differentiation_method = ps.FiniteDifference(order = 2)
 feature_library = ps.PolynomialLibrary(degree = 1, include_bias = False)
--- 5_sindy_mpc/test.py
+++ 5_sindy_mpc/test.py
@@ -1,17 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-rng = np.random.default_rng(seed = 488)
+list = []
 
-x = 2 # x0
-counter = 0
+for k in tqdm(range(10)):
+    rng = np.random.default_rng(seed = k)
 
-while (x**2 + 3 * x - 37) > 0.01 or (x**2 + 3 * x - 37) < -0.01:
-    r = (1 - (-1)) * rng.random() - 1
-    while x + r < 0 or x + r > 10:
-        r = (1 - (-1)) * rng.random() - 1
-    x = x + r
-    print(x)
-    counter += 1
+    x = 2 # x0
+    counter = 0
 
-print('Root: ' + str(x))
-print('There were ' + str(counter) + ' iterations.')
+    ball_size = 4
+
+    while (x**2 + 3 * x - 37) > 0.01 or (x**2 + 3 * x - 37) < -0.01:
+        r = (2 * ball_size) * rng.random() - ball_size # random number between -ball_size and ball_size, uniform distribution
+        while x + r < 0 or x + r > 10:
+            r = (2 * ball_size) * rng.random() - ball_size # random number between -ball_size and ball_size, uniform distribution
+        x = x + r
+        counter += 1
+
+    list.append(counter)
+
+plt.stem(list)
+plt.xlabel('RNG Seed')
+plt.ylabel('Number of Iterations')
+plt.title('Number of Iterations vs. Seed, Mean = ' + str(np.mean(list)))
+plt.show()
+
+print('debug')
